2461a807fcce6d5fabffe02ed401242ad6d10279
G Kraft <gabriel.kraft@hcahealthcare.com>
2021-11-04 19:32:10 -0500

    update

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
     }
   }
   required_version = ">= 0.14"
