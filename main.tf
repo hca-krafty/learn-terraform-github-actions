e90a3c011a73b1ab4b1bc4431993e041e2700db5
G Kraft <gabriel.kraft@hcahealthcare.com>
2021-11-04 19:16:22 -0500

    update

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/google"
-      version = "4.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 0.14"
-
   backend "remote" {
     organization = "hcaghk"
 
@@ -18,16 +6,4 @@ terraform {
       name = "gh-actions-demo"
     }
   }
-}
-
-provider "google" {
-  project = "hca-test-321221"
-  region = "us-central1"
-  zone = "us-central1-c"
-}
-
-resource "google_compute_network" "tf_vcp_net1" {
-  name = "tf-vpc-net-1"
-  routing_mode = "global"
-  auto_create_subnets = false
 }
