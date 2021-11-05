186e87d3909af1d1c3ad8998617b50743a7f01b7
G Kraft <gabriel.kraft@hcahealthcare.com>
2021-11-04 19:11:24 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
+      source  = "hashicorp/google"
+      version = "4.0.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "hcaghk"
 
     workspaces {
       name = "gh-actions-demo"
@@ -20,37 +20,14 @@ terraform {
   }
 }
 
-
-provider "aws" {
-  region = "us-west-2"
+provider "google" {
+  project = "hca-test-321221"
+  region = "us-central1"
+  zone = "us-central1-c"
 }
 
-
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "google_compute_network" "tf_vcp_net1" {
+  name = "tf-vpc-net-1"
+  routing_mode = "global"
+  auto_create_subnets = false
 }
